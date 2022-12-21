9ee59fca835d2c11fb2672fcf8f9fc82d41972d9
William Bucher <wjbuhcer3@gmail.com>
2022-12-20 23:03:10 -0500

    java version changed and removed cors

--- src/main/java/com/will/esp32api/controller/WebsiteController.java
+++ src/main/java/com/will/esp32api/controller/WebsiteController.java
@@ -1,5 +1,6 @@
 package com.will.esp32api.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,24 @@ public class WebsiteController {
 	
 	private int timesCalled = 0;
 	
-	@RequestMapping(path="update", method=RequestMethod.GET)
+	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+	@RequestMapping(path="/update", method=RequestMethod.GET)
 	public int updateTimesCalled() {
 		this.timesCalled = this.timesCalled + 1;
 		return this.timesCalled;
 	}
 	
-	@RequestMapping(path="get", method=RequestMethod.GET)
+	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+	@RequestMapping(path="/get", method=RequestMethod.GET)
 	public int getTimesCalled() {
 		return this.timesCalled;
 	}
+	
+	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public boolean test() {
+		return true;
+	}
+	
+	
 }
