543e5d56adf1c0f20612e4e166572368be4e9c09
William Bucher <wjbuhcer3@gmail.com>
2022-12-20 23:11:43 -0500

    changed package versions

--- src/main/java/com/will/esp32api/controller/WebsiteController.java
+++ src/main/java/com/will/esp32api/controller/WebsiteController.java
@@ -1,6 +1,5 @@
 package com.will.esp32api.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +11,20 @@ public class WebsiteController {
 	
 	private int timesCalled = 0;
 	
-	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+//	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
 	@RequestMapping(path="/update", method=RequestMethod.GET)
 	public int updateTimesCalled() {
 		this.timesCalled = this.timesCalled + 1;
 		return this.timesCalled;
 	}
 	
-	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+//	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
 	@RequestMapping(path="/get", method=RequestMethod.GET)
 	public int getTimesCalled() {
 		return this.timesCalled;
 	}
 	
-	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
+//	@CrossOrigin(origins = "https://willsbutton.netlify.app/")
 	@RequestMapping(path="/", method=RequestMethod.GET)
 	public boolean test() {
 		return true;
